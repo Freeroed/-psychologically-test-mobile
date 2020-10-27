f16b8ab24fed05515dd7029b0751e8712387336b
Kozlova Elena <lastonetwo18@gmail.com>
2020-10-27 16:47:44 +0300

    go to first question

--- app/src/main/java/com/example/cursach/TestDescriptionActivity.kt
+++ app/src/main/java/com/example/cursach/TestDescriptionActivity.kt
@@ -1,11 +1,19 @@
 package com.example.cursach
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class TestDescriptionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_description)
+
+        // клик по кнопке "Начать тест" - переход на страницу первого вопроса
+        goTest.setOnClickListener {
+            val intent = Intent(this, Question::class.java)
+            startActivity(intent)
+        }
     }
 }
