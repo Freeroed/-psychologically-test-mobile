a221fc1d72fe0ce3712761ee149ed61882084425
Kozlova Elena <lastonetwo18@gmail.com>
2020-10-27 21:39:29 +0300

    yes and no buttons

--- app/src/main/java/com/example/cursach/Question.kt
+++ app/src/main/java/com/example/cursach/Question.kt
@@ -4,10 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_question.*
 
 class Question : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
+
+        // выбор ответа "да"
+        selectedYes.setOnClickListener {
+            selectedYes.background =  getResources().getDrawable(R.color.selectedYelow)
+            selectedNo.background =  getResources().getDrawable(R.color.mainGreen)
+        }
+
+        // выбор ответа "нет"
+        selectedNo.setOnClickListener {
+            selectedYes.background =  getResources().getDrawable(R.color.mainGreen)
+            selectedNo.background =  getResources().getDrawable(R.color.selectedYelow)
+        }
     }
 }
