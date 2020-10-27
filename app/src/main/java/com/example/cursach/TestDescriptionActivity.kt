8ff208e1d8eab3914bf4d4ab8499688b21309f12
Kozlova Elena <lastonetwo18@gmail.com>
2020-10-27 16:32:01 +0300

    new activity and go to test description

---
+++ app/src/main/java/com/example/cursach/TestDescriptionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cursach
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestDescriptionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_description)
+    }
+}
